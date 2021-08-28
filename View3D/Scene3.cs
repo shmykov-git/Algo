809e1d0f4d23b07ffcbd4217d0293072fe9b8b76
Alexey Shmykov <shmykov.dev@gmail.com>
2021-08-28 14:19:23 +0300

    gradient

--- View3D/Scene3.cs
+++ View3D/Scene3.cs
@@ -77,8 +77,8 @@ namespace View3D
 
             // var shape = Parquets.PentagonalKershner8(0.05, 1.5).Rotate(-1.15).ToShape3().ToLines(40).AddVolumeZ(0.01);
 
-            // var shape = Surfaces.NormalDistribution(30, 30, 0.5, 0, 3.5).Rotate(Rotates.Z_Y).CenteredXZ().ApplyMaterialGradientY(Color.Black, Color.FromArgb(128, Color.Red));
-            var shape = Parquets.Triangles(12, 40, 0.1).Scale((Math.PI / 3.1, 3.0.Sqrt() / 1.7)).Move((Math.PI, -Math.PI / 2)).ToShape3().ToLines(20).AddVolumeZ(0.1).Transform(TransformFuncs3.HeartWrapZ).Rotate(Rotates.Z_Y).Scale(1, 1, 0.7).Rotate(Rotates.Y_mZ).ApplyMaterialGradientY(Color.FromArgb(23, 46, 200), Color.FromArgb(1, 240, 54));
+            var shape = Surfaces.NormalDistribution(50, 50, 0.6, 10, 3).Rotate(Rotates.Z_Y).CenteredXZ().ToMetaShape3(5, 5).ApplyMaterialGradientY(Color.Red, Color.White);
+            //var shape = Parquets.Triangles(12, 40, 0.1).Scale((Math.PI / 3.1, 3.0.Sqrt() / 1.7)).Move((Math.PI, -Math.PI / 2)).ToShape3().ToLines(20).AddVolumeZ(0.1).Transform(TransformFuncs3.HeartWrapZ).Rotate(Rotates.Z_Y).Scale(1, 1, 0.7).Rotate(Rotates.Y_mZ).ApplyMaterialGradientY(Color.FromArgb(23, 46, 200), Color.FromArgb(1, 240, 54));
 
             //var shape = Shapes.Cube.ToMetaShape3(1, 1, Color.Red, Color.Green);
 
