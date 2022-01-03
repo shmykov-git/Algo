fe86abfb42d38a07fda2491fddfc32807fe20d27
Alexey Shmykov <shmykov.dev@gmail.com>
2022-01-03 11:20:09 +0300

    news

--- Model3D/Libraries/Funcs3Z.cs
+++ Model3D/Libraries/Funcs3Z.cs
@@ -11,5 +11,7 @@ namespace Model3D.Libraries
         public static Func3Z Hyperboloid = (x, y) => x * x - y * y;
         public static Func3Z Paraboloid = (x, y) => x * x + y * y;
         public static Func3Z Waves = (x, y) => Math.Sin((x * x + y * y).Sqrt() *40)/50;
+        public static Func3Z Sphere = (x, y) => Math.Sqrt(Math.Abs(1 - x * x - y * y));
+        public static Func3Z SphereR(double r) => (x, y) => Math.Sqrt(Math.Abs(r * r - x * x - y * y));
     }
 }
