ca5ed6bdf7a977ba9d773ccc7c952debae93c35a
AlexeyShmykov <shmykov.dev@gmail.com>
2022-07-09 01:06:19 +0300

    1

--- View3D/Scene.cs
+++ View3D/Scene.cs
@@ -48,25 +48,25 @@ namespace View3D
         public Shape GetShape()
         {
 
-            var contentName = "w31";
+            var contentName = "g10";
 
             // заливка
 
             var options = new ShapeOptions()
             {
-                ZVolume = 0.02,                         // null - no volume, number - add volume to the shape
+                ZVolume = 0.005,                         // null - no volume, number - add volume to the shape
                 //ToLinesSize = 0.3,                      // .ToLines(number)
                 TriangulationStrategy = TriangulationStrategy.Trio,         // triangulation strategy
                 PolygonPointStrategy = PolygonPointStrategy.Circle,         // how to get points from single bitmap point
                 PolygonPointRadius = 0.03,              // radius of single point for some polygon strategies
                 //TriangulationFixFactor = 0.0001,
 
-                ColorLevel = 220,                       // 0 - white, 200 - middle, 255 - black
+                ColorLevel = 200,                       // 0 - white, 200 - middle, 255 - black
                 LevelStrategy = LevelStrategy.All,      // what kind of levels should be taken
                 SmoothOutLevel = 2,                     // number of 3 point smooth out process run
-                SmoothAngleScalar = 0.1,                // (on) -1:180%, -0.5:150%, 0:90%, 0.5:30%, 1:0% (off) on or off smoothing on 3 point condition
+                SmoothAngleScalar = 0.4,                // (on) -1:180%, -0.5:150%, 0:90%, 0.5:30%, 1:0% (off) on or off smoothing on 3 point condition
                 PolygonOptimizationLevel = 3,           // line center point skip. 0 - off, 1 - 3 points, 2 - 3 & 5 points, 3 - 3 & 5 & 7 points
-                MinimumPolygonPointsCount = 8,          // skip polygons with equal or less points
+                MinimumPolygonPointsCount = 0,          // skip polygons with equal or less points
 
                 //DebugPerimeters = true,                 // show perimeter information
                 //DebugBitmap = true,                     // show bitmap as chars
@@ -123,7 +123,7 @@ namespace View3D
                     //vectorizer.GetContentShape(contentName, centerOptions).MoveZ(0.05),
                     //Shapes.CoodsWithText.ApplyColor(Color.Black),
                     //Shapes.CirclePlatform(1,1,0.02)
-                    Shapes.SquarePlatform(2,2,0.04)
+                    //Shapes.SquarePlatform(2,2,0.04)
                 }.ToSingleShape();
         }
     }
