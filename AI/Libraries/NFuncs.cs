4488cdde411d646821c2acc289969b99227eca7c
AlexeyShmykov <shmykov.dev@gmail.com>
2024-06-01 00:33:21 +0400

    1

---
+++ AI/Libraries/NFuncs.cs
@@ -0,0 +1,12 @@
+﻿namespace AI.Libraries;
+
+public delegate float NFunc(float x);
+
+public static class NFuncs
+{
+    public static NFunc GetDampingFn(float c) => x => c * x;
+
+    public static NFunc GetSigmoidFn() => x => 1 / (1 + MathF.Exp(-x));
+
+    public static Func<float, float, float> GetErrorFn() => (x0, x) => (x0 - x) * x * (1 - x);
+}
