7a08ac152f269eb3139b72b944dcb2f7eb20defe
AlexeyShmykov <shmykov.dev@gmail.com>
2024-06-16 20:53:56 +0300

    1

--- AI/Model/NOptions.cs
+++ AI/Model/NOptions.cs
@@ -3,33 +3,33 @@
 namespace AI.Model;
 public class NOptions
 {
-    public int Seed { get; set; } = 0;
-    public double ShaffleFactor { get; set; } = 0.1;
-    public bool CleanupTrainTails { get; set; } = false;
-    public (int i, int j, double w)[][] Model { get; set; } = [];
-    public (int i, int j)[][] Graph { get; set; } = [];
-    public (int i, int j)[][] UpGraph { get; set; } = [];
-    public int[] Topology { get; set; } = [2, 5, 1];
-    public int[] UpTopology { get; set; } = [2, 5, 5, 1];
-    public bool AllowGrowing { get; set; } = false;
-    public (double a, double b) Weight0 { get; set; } = (2, -1);
-    public (double a, double b)? PowerWeight0 { get; set; } = null;
-    public NAct Act { get; set; } = NAct.Sigmoid;
-    public double SinA { get; set; } = 0.01;
-    public bool AllowBelief { get; set; } = true; // todo: remove
-    public int BeliefDeep { get; set; } = 3;
-    public double Power { get; set; } = 100;
-    public double SymmetryFactor { get; set; } = 0;
-    public int ParallelCount { get; set; } = 1;
-    public double LinkFactor { get; set; } = 0.2;
-    public double CrossLinkFactor { get; set; } = 0;
-    public double DampingCoeff { get; set; } = 0;
+    public int Seed { get; set; } = 0;                                  // use the same randomizing process to repeat creation and learning
+    public double ShaffleFactor { get; set; } = 0.1;                    // shaffle percent of training data for each learn circle
+    public bool CleanupTrainTails { get; set; } = false;                // clean some training data for each training (default = false)
+    public (int i, int j, double w)[][] Model { get; set; } = [];       // Learn model (default if set)
+    public (int i, int j)[][] Graph { get; set; } = [];                 // start topology graph (default if set)
+    public (int i, int j)[][] UpGraph { get; set; } = [];               // top topology graph to grow (default if set)
+    public int[] Topology { get; set; } = [2, 5, 1];                    // start topology
+    public int[] UpTopology { get; set; } = [2, 5, 5, 1];               // top topology to grow
+    public bool AllowGrowing { get; set; } = false;                     // use net growing
+    public (double a, double b) Weight0 { get; set; } = (2, -1);        // N.w0 = a + rnd * (b - a)
+    public (double a, double b)? PowerWeight0 { get; set; } = null;     // N.w0 = (a + rnd * (b - a)) / power (default if set)
+    public NAct Act { get; set; } = NAct.Sigmoid;       // activator function
+    public double SinA { get; set; } = 0.01;            // SinA activator value
+    public bool AllowBelief { get; set; } = false;      // use maximum number of top levels to learn
+    public int BeliefDeep { get; set; } = 3;            // maximum number of top levels to learn
+    public double Power { get; set; } = 100;            // use 'x * power' instead of x
+    public double SymmetryFactor { get; set; } = 0;     // 0 - off, 1 - e.dw should be avg for all training data
+    public int ParallelCount { get; set; } = 1;         // not implemented, try to use parallel calculations
+    public double LinkFactor { get; set; } = 0.2;       // percent of linked level edges
+    public double CrossLinkFactor { get; set; } = 0;    // not implemented
+    public double DampingCoeff { get; set; } = 0;       // use damping for each calculation (not investigated)
 
-    public double Nu { get; set; } = 0.1;
-    public double Alfa { get; set; } = 0.5;
+    public double Nu { get; set; } = 0.1;               // backpropagation parameter
+    public double Alfa { get; set; } = 0.5;             // backpropagation and power multiplicator parameter
 
-    public int UnwantedCount { get; set; } = 200000;
-    public double DynamicW0Factor { get; set; } = 0.1;
+    public int UnwantedCount { get; set; } = 200000;    // number of model computes to reach zero weight before remove it
+    public double DynamicW0Factor { get; set; } = 0.1;  // set w when new E added as avg sibling E multiplicator
 
-    public (int num, double[] input, double[] expected)[] Training { get; set; }
+    public (int num, double[] input, double[] expected)[] Training { get; set; }    // training data to learn
 }
